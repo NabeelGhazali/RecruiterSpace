112418f27264b9bea7b977f9d97c81f8da16bd03
Nabeel Ghazali <96319639+NabeelGhazali@users.noreply.github.com>
2022-11-29 20:45:57 -0500

    Add files via upload

---
+++ hardcoded_data.py
@@ -0,0 +1,31 @@
+departments = [
+               "Mechanical",
+               "Automotive",
+               "Electrical",
+               "Applied Computer Science",
+               "Electronic",
+               "Software",
+               "Management Information Systems",
+               "Mathematics",
+                ]
+domains = ['Python',
+            'C++',
+            'JAVA',
+            'Web development ',
+            'Android development',
+            'Data Science',
+            'AutoCad',
+            'Matlab',
+            'Proteus',
+            'PCB design',
+            'PSIM',
+            'R'
+                ]
+
+year = ['First Year', 'Second Year', 'Third Year',
+        'Fourth Year']
+
+
+departments_choices = [(k, k) for k in departments]
+domains_choices = [(k, k) for k in domains]
+year_choices = [(k, k) for k in year]
